608fa2fdfbc2259e5e165ce23d564f33041e3291
Satya Sanatan Bastia <121406330+satyasanatanbastia@users.noreply.github.com>
2023-09-17 09:21:43 +0000

    type conversion

---
+++ datatypeconversion.js
@@ -0,0 +1,16 @@
+// console.log("jay sri ram")
+// let  score = "498764avg"
+// console.log(typeof score);
+// let newscore = Number(score);
+// console.log(typeof newscore);
+// console.log(newscore);
+
+let a= "stya"
+let b= ""
+let c= true
+let d= false
+console.log(Boolean(a));
+console.log(Boolean(b));
+console.log(Boolean(c));
+console.log(Boolean(d));
+
